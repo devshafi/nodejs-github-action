e822631e5ec043cd3b82d01fc677b768c6d203b0
phshafi <shafi@programming-hero.com>
2022-09-09 23:51:54 +0600

    updated app

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-  console.log(`My super secret key is ${process.env.SUPER_SECRET_KEY}`);
+  console.log(`My super secret key is: ${process.env.SUPER_SECRET_KEY}`);
 })
