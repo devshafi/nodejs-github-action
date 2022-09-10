5e3107b7bf5e975c1ad797cad9d3bd177e7f2f65
phshafi <shafi@programming-hero.com>
2022-09-10 19:24:54 +0600

    added moment

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = 3210
+const moment =  require('moment');
 
 app.get('/', (req, res) => {
   res.send('Hello World!a')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port} ${moment()}` );
   console.log(`My super secret key is: ${process.env.SUPER_SECRET_KEY}`);
 })
